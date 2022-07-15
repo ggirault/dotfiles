37354dbd00a8f57ca9542d1dd73a4afdfea17c4f
ggirault <73304700+ggirault@users.noreply.github.com>
2022-07-15 17:14:52 +0200

    zsh: update fzf init

--- home/zsh/.config/zsh/lib/fzf.zsh
+++ home/zsh/.config/zsh/lib/fzf.zsh
@@ -1,3 +1,17 @@
+# # Setup fzf
+# ---------
+if [[ ! "$PATH" == *$HOMEBREW_PREFIX/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOMEBREW_PREFIX/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+
 # using ripgrep combined with preview
 # find-in-file - usage: fif <searchTerm>
 fif() {
