f204dc6f85aec486b0c17f0d63add6a0bbfce6e9
ggirault <73304700+ggirault@users.noreply.github.com>
2022-07-15 14:21:49 +0200

    zsh: use env var instead of brew command

--- home/zsh/.config/zsh/mac.zsh
+++ home/zsh/.config/zsh/mac.zsh
@@ -3,13 +3,13 @@
 export SHELL_SESSIONS_DISABLE=1
 
 # ZSH autosuggestion
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=063,underline'
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=40
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 
 # fast-syntax-hightlighting
-source $(brew --prefix)/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $HOMEBREW_PREFIX/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 
 if ! infocmp tmux > /dev/null 2>&1; then
   tic -ax "$ZDOTDIR"/tmux.terminfo
