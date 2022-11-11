4df285d1e809389916f3674a7aa8de8865e3ba23
ggirault <73304700+ggirault@users.noreply.github.com>
2022-11-11 18:58:22 +0100

    zsh: autosuggest history and completion

--- home/zsh/.config/zsh/mac.zsh
+++ home/zsh/.config/zsh/mac.zsh
@@ -7,6 +7,7 @@ source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=063,underline'
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=40
 ZSH_AUTOSUGGEST_USE_ASYNC=1
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # fast-syntax-hightlighting
 source $HOMEBREW_PREFIX/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
