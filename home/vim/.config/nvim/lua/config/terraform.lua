26d3d9e8aaed8b50d1d3dbabd1106d572a16a59f
ggirault <73304700+ggirault@users.noreply.github.com>
2023-01-25 11:06:39 +0100

    nvim: fix terraform lsp

--- home/vim/.config/nvim/lua/config/terraform.lua
+++ home/vim/.config/nvim/lua/config/terraform.lua
@@ -1,17 +1,20 @@
-require 'lspconfig'.tflint.setup()
+--require 'lspconfig'.tflint.setup()
 require 'lspconfig'.terraformls.setup{}
 vim.cmd([[
-  silent! autocmd! filetypedetect bufread,bufnewfile *.tflint
-  autocmd bufread,bufnewfile *.hcl set filetype=hcl
-  autocmd bufread,bufnewfile .terraformrc, terraform.rc set filetype=hcl
-  autocmd bufread,bufnewfile *.tf, *.tfvars set filetype=terraform
-  autocmd bufread,bufnewfile *.tfstate, *.tfstate.backup set filetype=json
+  silent! autocmd! filetypedetect BufRead,BufNewFile *.tflint
+  autocmd BufRead,BufNewFile *.hcl set filetype=hcl
+  autocmd BufRead,BufNewFile terraform.rc set filetype=hcl
+  autocmd BufRead,BufNewFile .terraformrc set filetype=hcl
+  autocmd BufRead,BufNewFile *.tf set filetype=terraform
+  autocmd BufRead,BufNewFile *.tfvars set filetype=terraform
+  autocmd BufRead,BufNewFile *.tfstate set filetype=json
+  autocmd BufRead,BufNewFile *.tfstate.backup set filetype=json
+  autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
+  autocmd BufWritePre *.tfvars lua vim.lsp.buf.formatting_sync()
 ]])
 
 -- auto-format on save
-vim.api.nvim_create_autocmd({'BufWritePre'}, {
-  pattern = { '*.tf', '*.tfvars' }
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
+--vim.api.nvim_create_autocmd({'BufWritePre'}, {
+--  pattern = { '*.tf', '*.tfvars' },
+--  callback = vim.lsp.buf.formatting_sync,
+--})
