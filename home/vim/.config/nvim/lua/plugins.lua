54205f0f520062bce1582d4f6ab10dc95fdbe62e
ggirault <73304700+ggirault@users.noreply.github.com>
2023-01-25 11:08:23 +0100

    nvim: add lsp plugins

--- home/vim/.config/nvim/lua/plugins.lua
+++ home/vim/.config/nvim/lua/plugins.lua
@@ -36,7 +36,7 @@ vim.cmd([[
 ]])
 
 -- Setup plugins
-return require('packer').startup({function(use)
+require('packer').startup({function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
@@ -93,10 +93,28 @@ return require('packer').startup({function(use)
   use { 'editorconfig/editorconfig-vim', config = [[require('config.editorconfig')]] }
 
   -- IaaC
-  --use { 'hashivim/vim-terraform', config = [[require('config.terraform')]] }
+  use { 'hashivim/vim-terraform', config = [[require('config.terraform')]] }
+
+  use { 'bash-lsp/bash-language-server', config = [[require'lspconfig'.bashls.setup{}]] }
+
+  use 'simrat39/rust-tools.nvim'
 end,
 config = {
   display = {
     open_fn = require('packer.util').float,
   }
 }})
+
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
